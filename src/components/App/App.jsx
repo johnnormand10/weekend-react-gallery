5c81b91473a30194a3820dca19f3b54cd2249389
Johnathan Normand <jnormand10@gmail.com>
2022-01-15 11:33:38 -0600

    added 'GET' into app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import './App.css';
+//importing useState
+import {useState, useEffect} from 'react';
+//import axios
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  const getImages = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((res) => {
+      console.log('Images received: ', res.data);
+
+      setGalleryList(res.data);
+    })
+    .catch((err) => {
+      console.error('Error retrieving images: ', err);
+    })
+  }
     return (
       <div className="App">
         <header className="App-header">
